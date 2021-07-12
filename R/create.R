d0cd22f957a0b3567d6a915b02f592cac28d61bd
William Claster <william108@gmail.com>
2021-07-12 17:19:11 +0900

    Ex 5.4.2 with just Dang and Soujanya

---
+++ R/create.R
@@ -0,0 +1,1 @@
+usethis::use_tutorial("Ex5.4.2", "Exercise for Section 5.4.2", open = interactive())
---
+++ creatingALearnrPackage.R
@@ -0,0 +1,8 @@
+#install.packages(c("devtools", "roxygen2", "learnr"))
+library(devtools)
+library(usethis)
+library(roxygen2)
+library(learnr)
+
+usethis::create_package("C:/Users/willi/Documents/clasterPackage")
+
