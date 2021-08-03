4f41f989fa2dad6c50fb4c8bf1983cfc0aedff78
William Claster <william108@gmail.com>
2021-08-03 12:30:21 +0900

    added in 541 and 531

--- R/create.R
+++ R/create.R
@@ -1,1 +1,26 @@
 usethis::use_tutorial("Ex5.4.2", "Exercise for Section 5.4.2", open = interactive())
+
+usethis::use_tutorial("Ex5.3.1", "Exercise for Section 5.3.1", open = interactive())
+
+usethis::use_tutorial("Ex5.4.1", "Exercise for Section 5.4.1", open = interactive())
+
+
+
+
+
+
+
+
+
+
+
+
+
+remotes::install_github("rstudio-education/gradethis") 
+
+
+
+
+
+
+
